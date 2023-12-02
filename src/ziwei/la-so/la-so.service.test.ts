0b3bae482ab9e91eb06ce64c09dd53b033c53643
HM <hieumdd@gmail.com>
2023-12-02 18:28:01 +0700

    feat: Update createLaSo

--- src/ziwei/la-so/la-so.service.test.ts
+++ src/ziwei/la-so/la-so.service.test.ts
@@ -3,4 +3,7 @@ import { createLaSo } from './la-so.service';
 it('createLaSo', () => {
     const laSo = createLaSo('1998-10-31');
     expect(laSo).toBeDefined();
+    expect(laSo.lunarYear.can.name).toBe('Mậu');
+    expect(laSo.lunarYear.chi.name).toBe('Dần');
+    expect(laSo.menh.name).toBe('Thổ');
 });
--- src/ziwei/la-so/la-so.service.ts
+++ src/ziwei/la-so/la-so.service.ts
@@ -1,8 +1,31 @@
 import { DateTime } from 'luxon';
 import { CalendarChinese } from 'date-chinese';
 
+import { getYearCan } from '../can/can.service';
+import { getYearChi } from '../chi/chi.service';
+import { getMenh } from '../menh/menh.service';
+
+const getLunarDate = (datetime: DateTime) => {
+    const lunarDate = new CalendarChinese().fromDate(datetime.toJSDate());
+    const [_, year, month, __, day] = lunarDate.get();
+
+    return { date: lunarDate, year, month, day };
+};
+
 export const createLaSo = (gregorianDateString: string) => {
     const gregorianDate = DateTime.fromISO(gregorianDateString);
-    const lunarDate = new CalendarChinese().fromDate(gregorianDate.toJSDate());
+    const lunarDate = getLunarDate(gregorianDate);
 
+    const lunarYear = {
+        can: getYearCan(gregorianDate.year),
+        chi: getYearChi(gregorianDate.year),
+    };
+    const menh = getMenh({ can: lunarYear.can, chi: lunarYear.chi });
+
+    return {
+        gregorianDate,
+        lunarDate,
+        lunarYear,
+        menh,
+    };
 };
