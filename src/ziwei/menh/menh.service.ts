8e5fa50a479d7c3671ea17d53b8f243dc8eafa12
HM <hieumdd@gmail.com>
2023-12-02 17:43:59 +0700

    feat: Add menh.service

---
+++ src/ziwei/menh/menh.service.ts
@@ -0,0 +1,12 @@
+import { Can } from '../can/can.type';
+import { Chi } from '../chi/chi.type';
+import { NguHanh } from '../ngu-hanh/ngu-hanh.type';
+import * as NguHanhs from '../ngu-hanh/ngu-hanh.enum';
+
+type GetMenhOptions = { can: Can; chi: Chi };
+
+export const getMenh = ({ can, chi }: GetMenhOptions) => {
+    const order = [NguHanhs.Kim, NguHanhs.Thuy, NguHanhs.Hoa, NguHanhs.Tho, NguHanhs.Moc];
+
+    return order[(can.menhRank + chi.menhRank) % 5] as NguHanh;
+};
