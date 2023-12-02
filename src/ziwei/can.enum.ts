ea143a22021773c2af112300542bea11acefb471
HM <hieumdd@gmail.com>
2023-12-02 13:12:32 +0700

    feat: Update can.enum

--- src/ziwei/can.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum Can {
-    Giap = 'Giap',
-}
---
+++ src/ziwei/can/can.enum.ts
@@ -0,0 +1,63 @@
+import { Can } from './can.type';
+import { AmDuong } from '../am-duong.enum';
+import { NguHanh } from '../ngu-hanh.enum';
+
+export const Giap: Can = {
+    name: 'Giáp',
+    amDuong: AmDuong.Duong,
+    nguHanh: NguHanh.Moc,
+}
+
+export const At: Can = {
+    name: 'Ất',
+    amDuong: AmDuong.Am,
+    nguHanh: NguHanh.Moc,
+}
+
+export const Binh: Can = {
+    name: 'Bính',
+    amDuong: AmDuong.Duong,
+    nguHanh: NguHanh.Hoa,
+}
+
+export const Dinh: Can = {
+    name: 'Đinh',
+    amDuong: AmDuong.Am,
+    nguHanh: NguHanh.Hoa,
+}
+
+export const Mau: Can = {
+    name: 'Mậu',
+    amDuong: AmDuong.Duong,
+    nguHanh: NguHanh.Tho,
+}
+
+export const Ky: Can = {
+    name: 'Kỷ',
+    amDuong: AmDuong.Am,
+    nguHanh: NguHanh.Tho,
+}
+
+export const Canh: Can = {
+    name: 'Canh',
+    amDuong: AmDuong.Duong,
+    nguHanh: NguHanh.Kim,
+}
+
+export const Tan: Can = {
+    name: 'Tân',
+    amDuong: AmDuong.Am,
+    nguHanh: NguHanh.Kim,
+}
+
+export const Nham: Can = {
+    name: 'Nhâm',
+    amDuong: AmDuong.Duong,
+    nguHanh: NguHanh.Thuy,
+}
+
+export const Quy: Can = {
+    name: 'Quý',
+    amDuong: AmDuong.Am,
+    nguHanh: NguHanh.Thuy,
+}
