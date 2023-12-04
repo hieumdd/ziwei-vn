f591b74c83611ac995c3e69cabc5ab8c8fe9e4e6
HM <hieumdd@gmail.com>
2023-12-04 14:47:55 +0700

    feat: Add cuc.type & cuc.enum

---
+++ src/ziwei/cuc/cuc.enum.ts
@@ -0,0 +1,32 @@
+import { Cuc } from './cuc.type';
+import * as NguHanhs from '../ngu-hanh/ngu-hanh.enum';
+
+export const ThuyNhiCuc: Cuc = {
+    name: 'Thuỷ Nhị Cục',
+    nguHanh: NguHanhs.Thuy,
+    rank: 2,
+};
+
+export const HoaLucCuc: Cuc = {
+    name: 'Hoả Lục Cục',
+    nguHanh: NguHanhs.Hoa,
+    rank: 6,
+};
+
+export const MocTamCuc: Cuc = {
+    name: 'Mộc Tam Cục',
+    nguHanh: NguHanhs.Moc,
+    rank: 3,
+};
+
+export const ThoNguCuc: Cuc = {
+    name: 'Thổ Ngũ Cục',
+    nguHanh: NguHanhs.Tho,
+    rank: 5,
+};
+
+export const KimTuCuc: Cuc = {
+    name: 'Kim Tứ Cục',
+    nguHanh: NguHanhs.Kim,
+    rank: 4,
+};
---
+++ src/ziwei/cuc/cuc.type.ts
@@ -0,0 +1,7 @@
+import { NguHanh } from "../ngu-hanh/ngu-hanh.type";
+
+export type Cuc = {
+    name: string;
+    nguHanh: NguHanh,
+    rank: number,
+}
