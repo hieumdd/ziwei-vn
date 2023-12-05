4b95188b49216610db4e45ef8a312a59800af3fd
HM <hieumdd@gmail.com>
2023-12-05 22:15:10 +0700

    feat: Add loc-ton.const

---
+++ src/ziwei/sao/phu-tinh/loc-ton.const.ts
@@ -0,0 +1,53 @@
+import { Sao } from '../sao.type';
+
+export const LocTon: Sao = {
+    name: 'Lộc Tồn',
+};
+
+export const BacSy: Sao = {
+    name: 'Bác Sỹ',
+};
+
+export const LucSy: Sao = {
+    name: 'Lực Sỹ',
+};
+
+export const ThanhLong: Sao = {
+    name: 'Thanh Long',
+};
+
+export const TieuHao: Sao = {
+    name: 'Tiểu Hao',
+};
+
+export const TuongQuan: Sao = {
+    name: 'Tướng Quân',
+};
+
+export const TauThu: Sao = {
+    name: 'Tấu Thư',
+};
+
+export const PhiLiem: Sao = {
+    name: 'Phi Liêm',
+};
+
+export const HyThan: Sao = {
+    name: 'Hỷ Thần',
+};
+
+export const BenhPhu: Sao = {
+    name: 'Bệnh Phù',
+};
+
+export const DaiHao: Sao = {
+    name: 'Đại Hao',
+};
+
+export const PhucBinh: Sao = {
+    name: 'Phục Binh',
+};
+
+export const QuanPhu: Sao = {
+    name: 'Quan Phủ',
+};
