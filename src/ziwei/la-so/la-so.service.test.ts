23d081dba4f10b62acf7f27954fc843609197d89
HM <hieumdd@gmail.com>
2023-12-05 20:53:57 +0700

    ci: Update test cases

--- src/ziwei/la-so/la-so.service.test.ts
+++ src/ziwei/la-so/la-so.service.test.ts
@@ -1,25 +1,52 @@
 import { createLaSo } from './la-so.service';
 
-it('createLaSo', () => {
-    const laSo = createLaSo('1998-10-31');
-    console.log(laSo);
+describe('createLaSo', () => {
+    let laSo: ReturnType<typeof createLaSo>;
 
-    expect(laSo).toBeDefined();
-    expect(laSo.lunarYear.can.name).toBe('Mậu');
-    expect(laSo.lunarYear.chi.name).toBe('Dần');
-    expect(laSo.menh.name).toBe('Thổ');
-    expect(laSo.cuc.name).toBe('Thuỷ Nhị Cục');
-    expect(laSo.diaChi.at(-1)?.isMenh).toBe(true);
-    expect(laSo.diaChi.at(9)?.isThan).toBe(true);
+    beforeAll(() => {
+        laSo = createLaSo('1998-10-31');
+    });
 
-    expect(laSo.diaChi[0].chinhTinh).toContainEqual({ name: 'Cự Môn' });
-    expect(laSo.diaChi[2].chinhTinh).toContainEqual({ name: 'Thiên Đồng' });
-    expect(laSo.diaChi[2].chinhTinh).toContainEqual({ name: 'Thiên Lương' });
-    expect(laSo.diaChi[5].chinhTinh.length).toBe(0);
-    expect(laSo.diaChi[7].chinhTinh).toContainEqual({ name: 'Tử Vi' });
-    expect(laSo.diaChi[7].chinhTinh).toContainEqual({ name: 'Phá Quân' });
-    expect(laSo.diaChi[9].chinhTinh).toContainEqual({ name: 'Thiên Phủ' });
+    describe('thien-ban', () => {
+        it('lunar-year', () => {
+            expect(laSo.lunarYear.can.name).toBe('Mậu');
+            expect(laSo.lunarYear.chi.name).toBe('Dần');
+        });
 
-    expect(laSo.diaChi[0].phuTinh).toContainEqual({ name: 'Điếu Khách' });
-    expect(laSo.diaChi[2].phuTinh).toContainEqual({ name: 'Thái Tuế' });
+        it('lunar-month', () => {
+            expect(laSo.lunarMonth.can.name).toBe('Nhâm');
+            expect(laSo.lunarMonth.chi.name).toBe('Tuất');
+        });
+
+        it('lunar-day', () => {
+            expect(laSo.lunarMonth.can.name).toBe('Tân');
+            expect(laSo.lunarMonth.chi.name).toBe('Hợi');
+        });
+
+        it('menh/cuc', () => {
+            expect(laSo.menh.name).toBe('Thổ');
+            expect(laSo.cuc.name).toBe('Thuỷ Nhị Cục');
+        });
+    });
+
+    describe('dia-chi', () => {
+        it('menh/than', () => {
+            expect(laSo.diaChi.at(-1)?.isMenh).toBe(true);
+            expect(laSo.diaChi.at(9)?.isThan).toBe(true);
+        });
+
+        it('chinh-tinh', () => {
+            expect(laSo.diaChi[0].chinhTinh).toContainEqual({ name: 'Cự Môn' });
+            expect(laSo.diaChi[2].chinhTinh).toContainEqual({ name: 'Thiên Đồng' });
+            expect(laSo.diaChi[2].chinhTinh).toContainEqual({ name: 'Thiên Lương' });
+            expect(laSo.diaChi[5].chinhTinh.length).toBe(0);
+        });
+
+        describe('phu-tinh', () => {
+            it('thai-tue', () => {
+                expect(laSo.diaChi[0].phuTinh).toContainEqual({ name: 'Điếu Khách' });
+                expect(laSo.diaChi[2].phuTinh).toContainEqual({ name: 'Thái Tuế' });
+            });
+        });
+    });
 });
