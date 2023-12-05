d5f1fc3ab83b84158b038c48bac1e6aa9e591157
HM <hieumdd@gmail.com>
2023-12-05 20:45:31 +0700

    feat: Remove can.service & chi.service

--- src/ziwei/can/can.service.ts
+++
@@ -1,8 +0,0 @@
-import rotate from 'rotate-array';
-
-import { CanTuple } from './can.tuple';
-import { neutralize } from '../ziwei.utils';
-
-export const getYearCan = (year: number) => {
-    return rotate([...CanTuple], -4)[neutralize(year, 10)];
-};
